4c1beb833ca2dabc520bd08e3026b555a4ae88c3
Irina Kondrateva <irakonrdratyeva@gmail.com>
2018-08-16 10:27:56 +0300

    removing initial folder

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
+const del = require('del');
 
 let [ ,, dirForSort = 'images', sortedDir = 'sortedImges', delInitDir = 'yes' ] = process.argv;
+let objDirFile = {};
+let countDirWithFiles = 0;
+let countRichedLastFile = 0;
 
 dirForSort = path.join(__dirname, dirForSort);
 sortedDir = path.join(__dirname, sortedDir);
-
-console.log(dirForSort, sortedDir);
 
 if (!fs.existsSync(dirForSort)) {
   console.error('Проверьте название папки для сортировки');
@@ -16,34 +18,35 @@ makeDir(sortedDir);
 
 const readDir = base => {
   fs.readdir(base, (err, files) => {
-
-    if (err) {
-      console.error(err.message);
-    }
+    writeError(err);
 
     files.forEach(item => {
       let localBase = path.join(base, item);
       
       fs.stat(localBase, (err, stats) => {
-
-        if (err) {
-          console.error(err.message);
-        }
+        writeError(err);
 
         if (stats.isDirectory()) {
           readDir(localBase);
         } else {
           let letterDir = path.join(sortedDir, item[0].toUpperCase());
-    
+          let filteredFile = files.filter(item => !!path.extname(item));
+
+          if (objDirFile[base] === undefined) ++countDirWithFiles;
+          objDirFile[base] = filteredFile[filteredFile.length - 1];
+          
           makeDir(letterDir);
           
           fs.copyFile(localBase, path.join(letterDir, item), err => {
-            if (err) {
-              console.error(err.message);
+            writeError(err);
+
+            if (item === objDirFile[base]) {
+              ++countRichedLastFile;
+              console.log(countDirWithFiles, countRichedLastFile, objDirFile);
             }
+
+            deleteDir();
           });
-  
-          console.log('File: ' + item);
         }
       });
     });
@@ -54,10 +57,21 @@ readDir(dirForSort);
 
 function makeDir (dirName) {
   if (!fs.existsSync(dirName)) {
-    fs.mkdir(dirName, err => {
-      if (err) {
-        console.error(err.message);
-      }
+    fs.mkdirSync(dirName);
+  }
+}
+
+function writeError (err) {
+  if (err) {
+    console.error(err.message);
+  }
+}
+
+function deleteDir () {
+  if (delInitDir === 'yes' && countDirWithFiles === countRichedLastFile && countDirWithFiles) {
+    console.log(countDirWithFiles, countRichedLastFile, objDirFile);
+    del([dirForSort]).then(paths => {
+      console.log('Deleted folder:\n', paths.join('\n'));
     });
   }
 }
