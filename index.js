8d25b12345930a7c4b3f78badf554fe0783d5b14
Irina Kondrateva <irakonrdratyeva@gmail.com>
2018-08-17 16:39:30 +0300

    удаление с использованием нативных методов Node.js

--- index.js
+++ index.js
@@ -1,51 +1,61 @@
 const fs = require('fs');
 const path = require('path');
-const del = require('del');
 
 let [ ,, dirForSort = 'images', sortedDir = 'sortedImges', delInitDir = 'yes' ] = process.argv;
 let objDirFile = {};
-let countDirWithFiles = 0;
-let countRichedLastFile = 0;
 
 dirForSort = path.join(__dirname, dirForSort);
 sortedDir = path.join(__dirname, sortedDir);
 
 if (!fs.existsSync(dirForSort)) {
   console.error('Проверьте название папки для сортировки');
+  process.exit(1);
 }
 
 makeDir(sortedDir);
 
-const readDir = base => {
-  fs.readdir(base, (err, files) => {
-    writeError(err);
+const readDir = dir => {
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.error('Невозможно прочитать содержимое папок', err.message);
+      process.exit(1);
+    }
+
+    if (!files.length) deleteDir(dir);
+
+    let filteredFile = files.filter(item => !!path.extname(item));
+    if (filteredFile.length) {
+      objDirFile[dir] = {
+        numFilesOfDir: filteredFile.length,
+        numDeletedFiles: 0
+      };
+    }
 
     files.forEach(item => {
-      let localBase = path.join(base, item);
+      let localDir = path.join(dir, item);
       
-      fs.stat(localBase, (err, stats) => {
-        writeError(err);
+      fs.stat(localDir, (err, stats) => {
+        if (err) {
+          console.error('Невозможно получить стат. данные', err.message);
+          process.exit(1);
+        }
 
         if (stats.isDirectory()) {
-          readDir(localBase);
+          readDir(localDir);
         } else {
           let letterDir = path.join(sortedDir, item[0].toUpperCase());
-          let filteredFile = files.filter(item => !!path.extname(item));
-
-          if (objDirFile[base] === undefined) ++countDirWithFiles;
-          objDirFile[base] = filteredFile[filteredFile.length - 1];
           
           makeDir(letterDir);
           
-          fs.copyFile(localBase, path.join(letterDir, item), err => {
-            writeError(err);
-
-            if (item === objDirFile[base]) {
-              ++countRichedLastFile;
-              console.log(countDirWithFiles, countRichedLastFile, objDirFile);
+          fs.copyFile(localDir, path.join(letterDir, item), err => {
+            if (err) {
+              console.error('Не удалось скопировать файл', err.message);
+              process.exit(1);
             }
 
-            deleteDir();
+            if (delInitDir === 'yes') {
+              delFilesDir(dir, item);
+            }
           });
         }
       });
@@ -55,23 +65,46 @@ const readDir = base => {
 
 readDir(dirForSort);
 
-function makeDir (dirName) {
-  if (!fs.existsSync(dirName)) {
-    fs.mkdirSync(dirName);
+function makeDir (dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
   }
 }
 
-function writeError (err) {
-  if (err) {
-    console.error(err.message);
-  }
+function delFilesDir (dir, file) {
+  fs.unlink(path.join(dir, file), err => {
+    if (err) {
+      console.error('Невозможно удалить файл', err.message);
+      return;
+    }
+    ++objDirFile[dir].numDeletedFiles;
+    
+    if (objDirFile[dir].numDeletedFiles !== objDirFile[dir].numFilesOfDir) return;
+    
+    deleteDir(dir);
+  });
 }
 
-function deleteDir () {
-  if (delInitDir === 'yes' && countDirWithFiles === countRichedLastFile && countDirWithFiles) {
-    console.log(countDirWithFiles, countRichedLastFile, objDirFile);
-    del([dirForSort]).then(paths => {
-      console.log('Deleted folder:\n', paths.join('\n'));
+function deleteDir (dir) {
+  fs.readdir(dir, (err, folders) => {
+    if (err) {
+      console.error('Ошибка при чтении директории для удаления', err.message);
+      return;
+    }
+
+    if (folders.length) return;
+    fs.rmdir(dir, err => {
+      if (err) {
+        console.error('Удаляемой папки уже не существует', err.message);
+        return;
+      }
+      console.log('Папка удалена!', dir);
+
+      if (path.dirname(dir) !== dirForSort) {
+        deleteDir(path.dirname(dir));
+      } else {
+        deleteDir(dirForSort);
+      }
     });
-  }
+  });
 }
